b54ec996706d9e9e5bad72f52ea44ed75f091617
wangxx10 <46438716+wangxx10@users.noreply.github.com>
2020-12-21 15:29:23 +0800

    update to match new

--- config.py
+++ config.py
@@ -2,11 +2,11 @@
 # -*- encoding: utf-8 -*-
 # 颜色
 # 可以参考网站 https://tool.oschina.net/commons?type=3 新增配置
-red = (255, 0, 0)   # 红色
-white = (250, 250, 250)  # 白色
-green = (0, 250, 0)  # 绿色
-blue = (0, 0, 250)  # 蓝色
-RosyBrown1 = (255, 193, 193)  #
+red = (255, 0, 0)               # 红色
+white = (250, 250, 250)         # 白色
+green = (0, 250, 0)             # 绿色
+blue = (0, 0, 250)              # 蓝色
+RosyBrown1 = (255, 193, 193)    #
 Sienna1 = (255, 130, 71)
 yellow = (255, 255, 0)
 Brown1 = (255, 64, 64)
@@ -15,3 +15,14 @@ HotPink1 = (255, 110, 180)
 grey31 = (79, 79, 79)
 Brown3 = (205, 51, 51)
 grey11 = (28, 28, 28)
+
+#pic_name = 'squirrel.jpg'        # 图片名称
+pic_name = '233.jpg'        # 图片名称
+word_color = HotPink1            # 文字颜色
+line_color = white               # 分隔线颜色
+split_count = 5                  # 分隔出多少（3）个区间 3**3 至少是2
+hard_level = 0                   # 游戏难度 0 任意两个互换  1 对角线互换  2 相邻互换
+
+
+if __name__ == '__main__':
+    pass
